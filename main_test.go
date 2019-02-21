31f664173a7848ec48266a8af75bc2b75fd898db
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-02-21 14:19:20 -0800

    table test written

--- main_test.go
+++ main_test.go
@@ -9,3 +9,22 @@ func TestCalculate(t *testing.T) {
 		t.Error("Expected 2 + 2 to equal 4")
 	}
 }
+
+func TestTableCalculate(t *testing.T) {
+	var tests = []struct {
+		input int
+		expected int
+	}{
+		{2, 4},
+		{6, 8},
+		{-10, -8},
+		{0, 2},
+		{100, 102},
+	}
+
+	for _, test := range tests {
+		if output := Calculate(test.input); output != test.expected {
+			t.Error("Test Failed: {} inputted, {} expected, recieved: {}", test.input, test.expected, output)
+		}
+	}
+}
